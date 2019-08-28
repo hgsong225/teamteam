8092a8f68022720fbc63c559d454912346fbd7e4
hgsong225 <themunt3@gmail.com>
2019-08-28 21:15:15 +0900

    edited home title

--- components/smart/home/Home.js
+++ components/smart/home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
         return (
             <div>
                 <Head>
-                    <title>팀팀 | 내 주변 축구 경기에 지금 당장 참여하세요. | 간편하게 축구 경기를 생성하고 편하게 사람을 모으세요.</title>
+                    <title>팀팀 | 내 주변 축구 경기 참여하기 | 간편하게 축구 할 사람 모으기</title>
                     <link href="../static/sign-in.css" rel="stylesheet" />
                 </Head>
                 teamteam에 오신 걸 환영합니다.
